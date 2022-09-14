e11a480feebbbf939afebd8b0c300dd0a8da9599
adam <adam.kud@wp.pl>
2022-09-14 19:49:04 +0200

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
+import { MainPageComponent } from './main-page/main-page.component';
+import { HeaderComponent } from './main-page/header/header.component';
+import { BannerComponent } from './main-page/banner/banner.component';
+import { PostsComponent } from './main-page/posts/posts.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MainPageComponent, HeaderComponent, BannerComponent, PostsComponent],
   imports: [BrowserModule, AppRoutingModule, ButtonModule],
   providers: [],
   bootstrap: [AppComponent],
